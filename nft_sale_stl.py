8cfeec9c8edb05dfeaaddfbc2cb169678004dbfe
eunice <windmoonwater@hotmail.com>
2022-06-11 13:58:56 -0700

    add nft transfer function

--- nft_sale_stl.py
+++ nft_sale_stl.py
@@ -1,3 +1,5 @@
+#EH: import library
+
 import streamlit as st
 import json
 import pandas as pd
@@ -6,7 +8,8 @@ import os
 from web3 import Web3
 from dotenv import load_dotenv
 from pathlib import Path
-w3 = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:7545'))
+
+w3 = Web3(Web3.HTTPProvider('HTTP:#127.0.0.1:7545'))
 
 
 #EH: load env file
@@ -28,7 +31,7 @@ st.set_page_config(page_title="NFT Approvals",layout="wide")
 def load_contract():
 
     # Load the contract ABI
-    with open(Path('./contracts/compiled/property_sale_abi.json')) as f:
+    with open(Path('./contracts/compiled/nft_sale_approval_abi.json')) as f:
         contract_abi = json.load(f)
 
     # Set the contract address (this is the address of the deployed contract)
@@ -75,83 +78,128 @@ public_key1=st.selectbox("Select Account", options=accounts)
 acct_bal=w3.eth.get_balance(public_key1)
 
 
+#EH: set trx count
 trx_count=w3.eth.get_transaction_count(public_key1)
 #st.write('trx_count:',trx_count)
 
 
-#EH:  inspection approval
+#EH:  inspection approval section
 with st.expander('************Inspection Section***************'):
     st.write('Inspector Account Balance:', acct_bal)
     inspector_private_key=st.text_input(label="Inspector private key")
     inspect_result=st.button(label='Inspection Appoval')
+
+    #EH: inspection apprval condition
     if inspect_result and len(inspector_private_key)>0:
         
-        
+        #EH: set payload
         payload={'from': public_key1, 'nonce': trx_count}
 
+        #EH: build raw trx from smart contract inspect function
         raw_inspect_txn=contract.functions.inspect().buildTransaction(payload)
+
+        #EH: sign trx
         signed_txn=w3.eth.account.signTransaction(raw_inspect_txn, private_key=inspector_private_key)
+
+        #EH: send/sign transaction
         inspect_trx=w3.eth.sendRawTransaction(signed_txn.rawTransaction)
         
+        #EH: display approval confirmation
         st.write('inspection approval confirmation:', inspect_trx)
-        #st.write('NFT authetication inspection status:',inspect_status)
-        st.write('asset address:',contract.functions.get_address().call())
 
+        #EH: display asset name
+        st.write('asset address:',contract.functions.get_nft().call())
+
+
+#EH:  display inspectation status
 if contract.functions.get_inspect_status().call() == True:
     inspect_status="Pass"
 else:
     inspect_status="Fail"
 st.write('NFT authetication inspection status:',inspect_status)
-        
 
-#EH:  Final Bidder Payment submission 
 
-seller_public_key="0xb8eF5cbB02657b79A5B9F98Abf44B119f218e9aC"   
+#EH:  Bidder Payment submission section
+
+#EH: set asset owner variable 
+
+asset_owner=contract.functions.owner().call()  
+   
 
 with st.expander('************Buyer trx Section***************'):
-    # if st.button('Payer submits Payment'): 
-        #if contract.functions.get_inspect_status().call()==True:
-            # public_key2=st.selectbox("Select Account", options=accounts)
+    #EH: get buyer private key
     buyer_private_key=st.text_input(label="buyer private key",placeholder=0*32)
 
-
+    #EH: check buyer account balance
     payer_acct_bal=w3.eth.get_balance(public_key1)
     st.write('Payer Account Balance:', acct_bal)
 
+    #EH: set payment submission button
     buyer_pay=st.button(label='Payer submits Payment')
 
     if buyer_pay:
-        payload={'from': public_key1, 'nonce': trx_count, 'value':(nft_bid_df['Bid amount'].iloc[0]).astype(int).item()}
 
-        raw_pay_txn=contract.functions.pay_property(public_key1).buildTransaction(payload)
+        #EH: set payload
+        payload={'from': public_key1,'nonce': trx_count, 'value':(nft_bid_df['Bid amount'].iloc[0]).astype(int).item()}
+
+        #EH: build raw trx from pay nft function
+        raw_pay_txn=contract.functions.pay_nft(public_key1).buildTransaction(payload)
+
+        #EH: sign trx
         signed_txn=w3.eth.account.signTransaction(raw_pay_txn, private_key=buyer_private_key)
+
+        #EH: send trx
         pay_trx=w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+
+        #EH: display confirmation
         st.write('Payer payment confirmation:', pay_trx)
+
+#EH: display payment date
 pay_date=datetime.fromtimestamp(contract.functions.get_pay_date(public_key1).call())
 st.write('Payment date:',pay_date)
 
-#EH: auction service approval
 
 
-# with st.expander('************Auction Service Approval***************'):
+# st.write(contract.all_functions()[0])
+st.write('contract address')
+st.write(contract.address)
+# st.write ('contract functions')
+#st.write(list(contract.functions))
 
-#     service_private_key=st.text_input(label="auction service private key")
-#     service_acct_bal=w3.eth.get_balance(public_key1)
-#     st.write('Auction service Account Balance:', acct_bal)
+#get nft
+if st.button(list(contract.functions)[1]):  st.write(contract.functions.get_nft().call())
+
+#owner
+if st.button(list(contract.functions)[2]):
     
-#     if len(pay_trx)>0:    
-                
-#         if st.button(label="Final Approval by auction service company"):     
-                
-#             payload={'from': public_key1, 'nonce': trx_count, 'value': 1001}
+    st.write(asset_owner)   
+    st.write(f'balance:{w3.eth.get_balance(asset_owner)}') 
 
-#             Buyer_public_key=st.text_input(label="Enter Buyer public key",placeholder=0*32)
+#get inspection status
+if st.button(list(contract.functions)[4]):  st.write(contract.functions.get_inspect_status().call()) 
 
 
-#             raw_final_approve_txn=contract.functions.lender_approve(Buyer_public_key).buildTransaction(payload)
-#             signed_txn=w3.eth.account.signTransaction(raw_final_approve_txn, private_key=service_private_key)
-#             service_approval_trx=w3.eth.sendRawTransaction(signed_txn.rawTransaction)
-# st.write('Final Approval by auction service confirmation:',service_approval_trx)
+#get pay date
+if st.button(list(contract.functions)[7]):  st.write(datetime.fromtimestamp(contract.functions.get_pay_date(public_key1).call()))
+
+#transfer asset
+final_private_key=st.text_input(label="nft receiver private key",placeholder=0*32)
+
+#EH: set transfer nft function
+if st.button(list(contract.functions)[3]):
+
+    #set  payload
+    payload={'from': public_key1, 'nonce': trx_count}
+
+    #EH: build trx
+    raw_inspect_txn=contract.functions.transfer_nft(public_key1).buildTransaction(payload)
+    #EH: sign trx
+    signed_txn=w3.eth.account.signTransaction(raw_inspect_txn, private_key=final_private_key)
+    #EH: send trx
+    transfer_trx=w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+    
+    #EH: display confirmation number
+    st.write('nft transfer confirmation:', transfer_trx)
 
 
 
